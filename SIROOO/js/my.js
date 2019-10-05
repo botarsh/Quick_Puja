8fa74f5ef4e36026b0e1f2fa4139da36eee9a37d
Madan Lal <36108781+NorinMp143@users.noreply.github.com>
2019-10-05 18:04:31 +0530

    remove some extra code
    I am removing some extra code form my.js file.

--- SIROOO/js/my.js
+++ SIROOO/js/my.js
@@ -11,26 +11,3 @@ function show(x){
                     icon.innerHTML='&#xe14c;';
                 }
             }
-            else if(x=='sidebar'){
-                sh = document.getElementById('sideHead');
-                ps = document.getElementById('project-show');
-                sh.style.display='none';
-                if(v.style.display=='block'){
-                    v.style.display='none';
-                    ps.style.display='block';
-                }
-                else{
-                    v.style.display='block';
-                    ps.style.display='none';
-                }
-            }
-        }
-        function fun(y){
-            c = document.getElementById(y);
-            if(c.style.display=='block'){
-                c.style.display='none';
-            }
-            else{
-                c.style.display='block';
-            }
-        }
